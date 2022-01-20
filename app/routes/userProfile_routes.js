c607025e67840fb371086f1ac44fce03c765e5b6
AWard59 <iamsancho5990@hotmail.com>
2022-01-19 18:47:44 -0800

    updated all profile routes to return profile data

--- app/routes/userProfile_routes.js
+++ app/routes/userProfile_routes.js
@@ -5,7 +5,6 @@ const passport = require('passport')
 
 // pull in Mongoose model for examples
 const UserProfile = require('../models/userProfile')
-const User = require('../models/user')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -52,14 +51,14 @@ router.get('/profile/:id', requireToken, (req, res, next) => {
 
 // CREATE
 // POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
+router.post('/profile', requireToken, (req, res, next) => {
   // set owner of new example to be current user
   req.body.example.owner = req.user.id
 
-  Example.create(req.body.example)
+  UserProfile.create(req.body.example)
     // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example: example.toObject() })
+    .then(profile => {
+      res.status(201).json({ profile: profile.toObject() })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -69,20 +68,20 @@ router.post('/examples', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/profile/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.example.owner
 
-  Example.findById(req.params.id)
+  UserProfile.findById(req.params.id)
     .then(handle404)
-    .then(example => {
+    .then(profile => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
-      requireOwnership(req, example)
+      requireOwnership(req, profile)
 
       // pass the result of Mongoose's `.update` to the next `.then`
-      return example.updateOne(req.body.example)
+      return profile.updateOne(req.body.profile)
     })
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
@@ -92,14 +91,14 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 
 // DESTROY
 // DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-  Example.findById(req.params.id)
+router.delete('/profile/:id', requireToken, (req, res, next) => {
+  UserProfile.findById(req.params.id)
     .then(handle404)
-    .then(example => {
+    .then(profile => {
       // throw an error if current user doesn't own `example`
-      requireOwnership(req, example)
+      requireOwnership(req, profile)
       // delete the example ONLY IF the above didn't throw
-      example.deleteOne()
+      profile.deleteOne()
     })
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
